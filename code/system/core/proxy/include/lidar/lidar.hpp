688fed4cb318ee11064895c3f832de062a7f7944
MatsJSvensson <matsjsvensson@gmail.com>
2016-04-25 09:19:18 +0200

    Adding "working" interface for Lidar

--- code/system/core/proxy/include/lidar/lidar.hpp
+++ code/system/core/proxy/include/lidar/lidar.hpp
@@ -21,9 +21,13 @@
 #define LIDAR_LIDAR_HPP_
 
 #include <memory>
+#include <list>
 
 #include "opendavinci/odcore/base/module/TimeTriggeredConferenceClientModule.h"
 #include "opendavinci/odcore/data/Container.h"
+#include "opendavinci/odcore/wrapper/SerialPort.h"
+
+#include "opendlvdata/GeneratedHeaders_opendlvdata.h"
 
 namespace opendlv {
 namespace proxy {
@@ -34,19 +38,38 @@ class Device;
 /**
  * This class provides...
  */
-class Lidar : public odcore::base::module::TimeTriggeredConferenceClientModule {
+class Lidar : public odcore::base::module::TimeTriggeredConferenceClientModule,
+              public odcore::io::StringListener {
  public:
   Lidar(int32_t const &, char **);
   Lidar(Lidar const &) = delete;
   Lidar &operator=(Lidar const &) = delete;
   virtual ~Lidar();
+
   odcore::data::dmcp::ModuleExitCodeMessage::ModuleExitCode body();
+  virtual void nextString(const std::string &s);
+  virtual void SendData();
+  virtual void ConvertToDistances();
+  virtual void WriteToFile();
 
  private:
   void setUp();
   void tearDown();
 
+  bool m_firstHeader;
+  bool m_indicator;
+  bool m_startConfirmed;
+  uint32_t m_counter;
+  std::vector<opendlv::coordinate::Spherical3> m_freshCoordinates;
+  double m_distances[361];
+  double m_position[3];
+  unsigned char m_measurements[1000];
+  unsigned char m_startResponse[10];
+  unsigned char m_measurementHeader[7];
+  unsigned char m_buffer[10];
+
   std::unique_ptr<Device> m_device;
+  std::shared_ptr<odcore::wrapper::SerialPort> m_sick;
 };
 
 } // lidar
--- code/system/core/proxy/src/lidar/lidar.cpp
+++ code/system/core/proxy/src/lidar/lidar.cpp
@@ -21,12 +21,14 @@
 #include <cstring>
 #include <cmath>
 #include <iostream>
+#include <fstream>
 
 #include "opendavinci/odcore/base/KeyValueConfiguration.h"
 #include "opendavinci/odcore/data/Container.h"
 #include "opendavinci/odcore/data/TimeStamp.h"
-
-#include "opendlvdata/GeneratedHeaders_opendlvdata.h"
+#include "opendavinci/odcore/base/Thread.h"
+#include "opendavinci/odcore/wrapper/SerialPort.h"
+#include "opendavinci/odcore/wrapper/SerialPortFactory.h"
 
 #include "lidar/lidar.hpp"
 #include "lidar/device.hpp"
@@ -43,7 +45,13 @@ namespace lidar {
   */
 Lidar::Lidar(int32_t const &a_argc, char **a_argv)
     : TimeTriggeredConferenceClientModule(a_argc, a_argv, "proxy-lidar")
+    , m_firstHeader(true)
+    , m_indicator(true)
+    , m_startConfirmed(false)
+    , m_counter(0)
+    , m_freshCoordinates()
     , m_device()
+    , m_sick()
 {
 }
 
@@ -54,7 +62,9 @@ Lidar::~Lidar()
 // This method will do the main data processing job.
 odcore::data::dmcp::ModuleExitCodeMessage::ModuleExitCode Lidar::body()
 {
-
+  if(m_startConfirmed) {
+    SendData();
+  }
   // Send opendlv::proxy::SphericalTimeOfFlight ??
 
   return odcore::data::dmcp::ModuleExitCodeMessage::OKAY;
@@ -65,12 +75,11 @@ void Lidar::setUp()
   odcore::base::KeyValueConfiguration kv = getKeyValueConfiguration();
 
   std::string const type = kv.getValue<std::string>("proxy-lidar.type");
-  /*  std::string const port =
-    kv.getValue<std::string>("proxy-lidar.port");
-    float const mountX = kv.getValue<float>("proxy-lidar.mount.x");
-    float const mountY = kv.getValue<float>("proxy-lidar.mount.y");
-    float const mountZ = kv.getValue<float>("proxy-lidar.mount.z");
-  */
+
+  m_position[0] = kv.getValue<float>("proxy-lidar.mount.x");
+  m_position[1] = kv.getValue<float>("proxy-lidar.mount.y");
+  m_position[2] = kv.getValue<float>("proxy-lidar.mount.z");
+
   if (type.compare("sick") == 0) {
     //      m_device = std::unique_ptr<Device>(new SickDevice());
   }
@@ -79,10 +88,140 @@ void Lidar::setUp()
     std::cerr << "[proxy-lidar] No valid device driver defined."
               << std::endl;
   }
+
+  string SERIAL_PORT = kv.getValue<std::string>("proxy-lidar.port");
+  uint32_t BAUD_RATE = 9600; //TODO: Put in configuration file
+
+  try {
+    m_sick = shared_ptr<odcore::wrapper::SerialPort>(odcore::wrapper::SerialPortFactory::createSerialPort(SERIAL_PORT, BAUD_RATE));
+    m_sick->setStringListener(this);
+    m_sick->start();
+  }
+  catch(string &exception) {
+    cerr << "[" << getName() << "] Could not connect to Sickan: " << exception << endl;
+  }
+
+  m_startResponse[0] = 0x06;
+  m_startResponse[1] = 0x02;
+  m_startResponse[2] = 0x80;
+  m_startResponse[3] = 0x03;
+  m_startResponse[4] = 0x00;
+  m_startResponse[5] = 0xA0;
+  m_startResponse[6] = 0x00;
+  m_startResponse[7] = 0x10;
+  m_startResponse[8] = 0x16;
+  m_startResponse[9] = 0x0A;
+  m_measurementHeader[0] = 0x02;
+  m_measurementHeader[1] = 0x80;
+  m_measurementHeader[2] = 0xD6;
+  m_measurementHeader[3] = 0x02;
+  m_measurementHeader[4] = 0xB0;
+  m_measurementHeader[5] = 0x69;
+  m_measurementHeader[6] = 0x41;
+}
+
+void Lidar::nextString(const std::string &s) 
+{
+  unsigned char byte = (unsigned char)s[0]; //Storing byte as unsigned char
+
+  //Updating buffer
+  for(uint32_t i = 0; i < 9; i++) { 
+    m_buffer[i] = m_buffer[i+1]; 
+  }
+  m_buffer[9] = byte;
+
+  m_measurements[m_counter] = byte;
+  m_counter++;
+
+  m_indicator = true; //Checking for start confirmation
+  for(uint32_t i = 0; i < 10; i++) {
+    if(m_buffer[i] != m_startResponse[i]) { m_indicator = false; }        
+  }
+  if(m_indicator) { 
+    m_counter = 0;
+    m_startConfirmed = true; 
+  }
+
+  m_indicator = true; //Checking for output header
+  for(uint32_t i = 0; i < 7; i++) {
+    if(m_buffer[i] != m_measurementHeader[i]) { m_indicator = false; }
+  }
+  if(m_indicator) {
+    if(!m_firstHeader) {
+      ConvertToDistances();
+    } 
+    m_counter = 0;  
+  }
+
+}
+
+void Lidar::ConvertToDistances()
+{
+  uint32_t byte1;
+  uint32_t byte2;
+  uint32_t distance;
+  double cartesian[2];
+  double PI = 3.14159265;
+
+  m_freshCoordinates.clear();
+
+  for(uint32_t i = 0; i < 361; i++) {
+
+    byte1 = (int)m_measurements[i*2];
+    byte2 = (int)m_measurements[i*2+1];
+    distance = byte1*32 + byte2/8; //Integer millimeters in local polar coordinates
+    cartesian[0] = distance * sin(PI * i /360) / 1000; //Local cartesian coordinates in meters
+    cartesian[1] = distance * (-cos(PI * i /360)) / 1000;
+    cartesian[0] += m_position[0]; //Truck cartesian coordinates
+    cartesian[1] += m_position[1];
+
+    float p[3];
+    p[0] = sqrt(pow(cartesian[0],2) + pow(cartesian[1],2));
+    p[1] = atan(cartesian[1]/cartesian[0]);
+    p[2] = 0;
+    opendlv::coordinate::Spherical3 spherical(p);
+    m_freshCoordinates.push_back(spherical);
+
+    m_distances[i] = p[0];
+    if(i == 0) {
+      std::cout << distance << " ";
+    }
+
+  }
+
+  WriteToFile();
+
+}
+
+void Lidar::SendData()
+{
+  opendlv::proxy::SphericalTimeOfFlight nextReading;
+  nextReading.setListOfPoints(m_freshCoordinates);
+
+  odcore::data::Container c(nextReading);
+  getConference().send(c);
+}
+
+void Lidar::WriteToFile()
+{
+  std::ofstream write;
+  write.open("Test.txt",fstream::out);
+
+
+  for(uint32_t i = 0; i < 361; i++)
+  {
+    write << m_distances[i] << ' ';
+  }
+  write << std::endl;
+
+
+  write.close();
 }
 
 void Lidar::tearDown()
 {
+  m_sick->stop();
+  m_sick->setStringListener(NULL);
 }
 
 } // lidar
